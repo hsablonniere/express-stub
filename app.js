f140fea19f2179fe226811c0ed145d0cd12eb937
Quentin ADAM <quentin.adam@waxzce.org>
2017-12-26 12:01:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = equire('cookie-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressStatsd = require('express-statsd');
 
